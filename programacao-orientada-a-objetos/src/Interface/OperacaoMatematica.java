f0e241e1f38220ce39aff9e3a2696e3acbae0080
Bruno <brunopaulsenfagundes@gmail.com>
2022-11-11 15:17:19 -0300

    aula herança, associação e interface

---
+++ programacao-orientada-a-objetos/src/Interface/OperacaoMatematica.java
@@ -0,0 +1,12 @@
+package Interface;
+
+interface OperacaoMatematica {
+
+    void soma(double n1, double n2);
+
+    void subtracao(double n1, double n2);
+
+    void multiplicacao(double n1, double n2);
+
+    void divisao(double n1, double n2);
+}
